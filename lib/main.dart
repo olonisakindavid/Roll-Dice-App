73c02f82230b9b7878f8c605e9c4a30757e0bd9b
Olonisakin David <olonisakin.david@stu.cu.edu.ng>
2023-08-22 18:44:16 +0100

    constructor function

--- lib/main.dart
+++ lib/main.dart
@@ -2,37 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: GradientContainer()
-      ),
+    const MaterialApp(
+      home: Scaffold(body: GradientContainer()),
     ),
   ); //root widget , using name argument,
 }
 
 class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
   @override
   Widget build(context) {
     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 168, 88, 143),
-                Color.fromARGB(255, 44, 100, 168)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 168, 88, 143),
+            Color.fromARGB(255, 44, 100, 168)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
           ),
         ),
+      ),
+    );
   }
 }  //for normal classes i wont need the statelesswidget
