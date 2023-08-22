34a0958390e189a95be6781818c3f6ef19c8b64f
Olonisakin David <olonisakin.david@stu.cu.edu.ng>
2023-08-22 23:40:46 +0100

    custom styled text

---
+++ lib/gradient_container.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 168, 88, 143),
+            Color.fromARGB(255, 44, 100, 168)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(child: StyledText()),
+    );
+  }
+}  //for normal classes i wont need the statelesswidget
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:roll_dice_app/gradient_container.dart';
+import 'package:roll_dice_app/styled_text.dart';
+
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: GradientContainer()),
+      home: Scaffold(body: GradientContainer(), body: StyledText()),
     ),
   ); //root widget , using name argument,
 }
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 168, 88, 143),
-            Color.fromARGB(255, 44, 100, 168)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
-}  //for normal classes i wont need the statelesswidget
