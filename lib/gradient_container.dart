3b002b68648be75013c6b7e5a786df0f0a429b32
Olonisakin David <olonisakin.david@stu.cu.edu.ng>
2023-08-25 23:22:08 +0100

    reusable gradient container, 2 colors

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,22 +5,43 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 168, 88, 143),
-            Color.fromARGB(255, 44, 100, 168)
-          ],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText()),
+      child: const Center(child: StyledText('Djonesplays')),
     );
   }
 }  //for normal classes i wont need the statelesswidget
+
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(child: StyledText('Djonesplays')),
+//     );
+//   }
+// }  //for normal classes i wont need the statelesswidget
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(
+            Color.fromARGB(255, 59, 30, 226), Color.fromARGB(255, 130, 50, 23)),
       ),
     ),
   ); //root widget , using name argument,
