fde90735535cfd974f74d4e5d1402afcdc53d68f
Olonisakin David <olonisakin.david@stu.cu.edu.ng>
2023-08-26 00:04:41 +0100

    centered and increase font size

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -25,13 +25,21 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/dice-2.png',
               width: 200,
             ),
+            const SizedBox(height: 20),
             TextButton(
               onPressed: rollDice,
+              style: TextButton.styleFrom(
+                  // padding: const EdgeInsets.only(
+                  //   top: 20,
+                  // ),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 28)),
               child: const Text('Roll Dice'),
             )
           ],
